b55dd4ae3b624f67c78a5ebd2fa16d836b3f2c40
Francois <francois.marion4@gmail.com>
2020-01-16 17:25:44 +0100

    MàJ Exercice, création Session

--- src/app/model/session.ts
+++ src/app/model/session.ts
@@ -1,3 +1,23 @@
-export class Session {
+import {Program} from './program';
+import {Exercice} from './exercice';
 
+export class Session {
+  id: number;
+  version: number;
+  name: string;
+  text: string;
+  nbSession: number;
+  program: Program;
+  exercices: Array<Exercice> = new Array<Exercice>();
+
+  constructor(id?: number, version?: number, name?: string, text?: string,
+              nbSession?: number, program?: Program, exercices?: Array<Exercice>) {
+    this.id = id;
+    this.version = version;
+    this.name = name;
+    this.text = text;
+    this.nbSession = nbSession;
+    this.program = program;
+    this.exercices = exercices;
+  }
 }
