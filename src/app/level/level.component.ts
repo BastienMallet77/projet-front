8f473fce031f722fdeed1ea1aa545dd610d9e9da
beren <berengere.vallier@hotmail.fr>
2020-01-16 15:54:13 +0100

    composant level, service level,
    navbar

--- src/app/level/level.component.ts
+++ src/app/level/level.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {LevelHttpService} from "./level-http.service";
+import {LevelService} from "./level.service";
+import {Level} from "../model/level";
 
 @Component({
   selector: 'level',
@@ -6,10 +10,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./level.component.css']
 })
 export class LevelComponent implements OnInit {
+  currentLevel: Level = null;
+  modalLevel: Level = null;
 
-  constructor() { }
+  constructor(private modalService: NgbModal, private levelHttpService: LevelHttpService) {
+  }
 
   ngOnInit() {
+  }
+
+  list() {
+    return this.levelHttpService.findAll();
+  }
+
+  add() {
+    this.currentLevel = new Level();
+  }
+
+  detail(content, id: number) {
+    this.levelHttpService.findById(id).subscribe(resp => {
+      this.modalLevel = resp;
+    }, error => {
+      console.log(error);
+    });
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', scrollable: true});
+  }
+
+  edit(id: number) {
+    this.levelHttpService.findById(id).subscribe(resp => {
+      this.currentLevel = resp;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  save() {
+    this.levelHttpService.save(this.currentLevel);
+    this.cancel();
+  }
+
+  delete(id: number) {
+    this.levelHttpService.delete(id);
+  }
+
+  cancel() {
+    this.currentLevel = null;
+  }
+
+ next(level: Level) {
+    this.levelHttpService.next(level);
   }
 
 }
---
+++ src/app/model/specialisation.ts
@@ -0,0 +1,6 @@
+export class Specialisation {
+
+
+  constructor() {
+  }
+}
