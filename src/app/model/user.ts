bc32391e20db2da05ac2054611ec31911f188b5a
AD <albertderarsenian@gmail.com>
2020-01-16 12:28:43 +0100

    FINAL User V2 (class)

--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -18,4 +18,21 @@ export class User {
   programs: Program;
   inProgress: InProgress;
   degreesCoach: Degree;
+
+
+  constructor(id?: number, version?: number, firstName?: string, lastName?: string, userName?: string, email?: string, password?: string, birthDate?: Date, commercial?: boolean, isDesactivated?: boolean, programs?: Program, inProgress?: InProgress, degreesCoach?: Degree) {
+    this.id = id;
+    this.version = version;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.userName = userName;
+    this.email = email;
+    this.password = password;
+    this.birthDate = birthDate;
+    this.commercial = commercial;
+    this.isDesactivated = isDesactivated;
+    this.programs = programs;
+    this.inProgress = inProgress;
+    this.degreesCoach = degreesCoach;
+  }
 }
