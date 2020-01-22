e76f4b82734033eb404b8d43d7ad4091272913cc
Batou <bastien.dd@aliceadsl.fr>
2020-01-22 17:41:01 +0100

    corrections mineures

--- src/app/program-board-view/program-board-view-http.service.ts
+++ src/app/program-board-view/program-board-view-http.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable, Input} from '@angular/core';
 import {Observable} from "rxjs";
 import {Program} from "../model/program";
 import {AppConfigService} from "../app-config.service";
